09b3fb20e3ae77aef3f18f690a46e11f333ae398
cela <1319239848@qq.com>
2019-07-25 15:49:20 +0800

    DFS模板

---
+++ src/com/zk/DfsTemplate.java
@@ -0,0 +1,32 @@
+package com.zk;
+
+
+/**
+ * @auther ZhangKe
+ * @date 2019/7/25 15:31
+ * Depth-First-Search模板
+ */
+public class DfsTemplate {
+
+    /*int check(参数)
+    {
+        if(满足条件)
+            return 1;
+        return 0;
+    }
+
+    void dfs(int step)
+    {
+        判断边界
+        {
+            相应操作
+        }
+        尝试每一种可能
+        {
+            满足check条件
+                    标记
+            继续下一步dfs(step+1)
+            恢复初始状态（回溯的时候要用到）
+        }
+    }*/
+}
